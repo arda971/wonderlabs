54cde93fc16acc5302aa9ee5b2bd34252455febb
pyc <pyc971@gmail.com>
2018-08-06 21:38:31 -0400

    add controller

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var auth = require('./routes/auth');
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const passport = require('passport');
+
 
 var app = express();
 
@@ -24,34 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', auth);
 
 
-// Set up passport strategy
-passport.use(new GoogleStrategy(
-  {
-    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
-    callbackURL: 'https://wonder971.herokuapp.com/auth/google/callback',
-    scope: ['email'],
-  },
-  // This is a "verify" function required by all Passport strategies
-  (accessToken, refreshToken, profile, cb) => {
-    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
-    return cb(null, profile);
-  },
-));
 
-app.get('/auth/google', passport.authenticate('google'));
 
-// This is where Google sends users once they authenticate with Google
-// Make sure this endpoint matches the "callbackURL" from step 4.2 and the "authorized redirect URI" from Step 3
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/', session: false }),
-  (req, res) => {
-    console.log('wooo we authenticated, here is our user object:', req.user);
-    res.json(req.user);
-  }
-);
+
 
 
 // catch 404 and forward to error handler
---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var passportFacebook = require('../controller/facebookAuth');
+var passportGoogle = require('../controller/googleAuth');
+
+
+/* LOGIN ROUTER */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Please Sign In with:' });
+});
+
+
+/* LOGOUT ROUTER */
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+/* FACEBOOK ROUTER */
+router.get('/facebook',
+  passportFacebook.authenticate('facebook'));
+
+router.get('/facebook/callback',
+  passportFacebook.authenticate('facebook', { failureRedirect: '/auth/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
+
+/* GOOGLE ROUTER */
+
+app.get('/auth/google', passportGoogle.authenticate('google'));
+
+// This is where Google sends users once they authenticate with Google
+// Make sure this endpoint matches the "callbackURL" from step 4.2 and the "authorized redirect URI" from Step 3
+app.get('/auth/google/callback',
+  passportGoogle.authenticate('google', { failureRedirect: '/', session: false }),
+  (req, res) => {
+    console.log('wooo we authenticated, here is our user object:', req.user);
+    res.json(req.user);
+  }
+);
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
