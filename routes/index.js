98db4da65668163867328e121aa73dfaa5695d8b
pyc <pyc971@gmail.com>
2018-08-08 12:30:49 -0400

    fix register template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',user:req.user.name });
+  res.render('index', { title: 'Express',user:req.user });
   console.log('usr',req.user)
 });
 
