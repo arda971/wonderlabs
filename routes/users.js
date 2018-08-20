b78487e64065f11ae3d9d9f10d04fd0a3665aee5
pyc <pyc971@gmail.com>
2018-08-20 11:33:13 -0400

    add projects

--- routes/users.js
+++ routes/users.js
@@ -112,7 +112,17 @@ router.get('/project/:id', accessProtectionMiddleware,function(req, res, next) {
 
 });
 
+});
 
+router.post('/project/:id', accessProtectionMiddleware,function(req, res, next) {
+
+  User.findByIdAndUpdate({_id:req.params.id}, req.body, function (err, project) {
+
+    console.log('project update',project);
+    res.redirect('/users/projects');
+    
+
+});
 
 
 });
